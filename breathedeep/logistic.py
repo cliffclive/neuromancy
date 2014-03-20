2f1dd51b7f4ae6956371104697d878074990b7ef
ccc <cliffclive@hotmail.com>
2014-03-19 21:40:32 -0500

    Lost track of which computers are out of sync, so I'm committing everything I have.

--- breathedeep/logistic.py
+++ breathedeep/logistic.py
@@ -159,7 +159,7 @@ def load_data(dataset):
     data_dir, data_file = os.path.split(dataset)
     if data_dir == "" and not os.path.isfile(dataset):
         # Check if dataset is in the data directory.
-        new_path = os.path.join(os.path.split(__file__)[0], "..", "data", dataset)
+        new_path = os.path.join(os.path.split(__file__)[0], "data", dataset)
         if os.path.isfile(new_path) or data_file == 'mnist.pkl.gz':
             dataset = new_path
 
@@ -220,7 +220,7 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
                            dataset='mnist.pkl.gz',
                            batch_size=600):
     """
-    Demonstrate stochastic gradient descent optimization of a log-linear
+    Demonstrate stochastic gradient descent (SGD) optimization of a log-linear
     model
 
     This is demonstrated on MNIST.
@@ -306,15 +306,12 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
     print '... training the model'
     # early-stopping parameters
     patience = 5000  # look as this many examples regardless
-    patience_increase = 2  # wait this much longer when a new best is
-                                  # found
+    patience_increase = 2  # wait this much longer when a new best is found
     improvement_threshold = 0.995  # a relative improvement of this much is
-                                  # considered significant
+                                   # considered significant
     validation_frequency = min(n_train_batches, patience / 2)
-                                  # go through this many
-                                  # minibatche before checking the network
-                                  # on the validation set; in this case we
-                                  # check every epoch
+            # go through this many minibatches before checking the network
+            # on the validation set; in this case we check every epoch
 
     best_params = None
     best_validation_loss = numpy.inf
